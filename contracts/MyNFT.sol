603f11e0a37a2b23f05e855e431d6424875feac2
Antz0x0z <47379684+Antz0x0z@users.noreply.github.com>
2021-04-06 16:17:03 +0700

    Add tansfer mode
    Enable ERC721 NFT-transfer mode

--- contracts/MyNFT.sol
+++ contracts/MyNFT.sol
@@ -2,27 +2,33 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.7.3;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+// import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 
 contract MyNFT is ERC721, Ownable {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
+    mapping (address => mapping (address => uint256)) internal allowed;
+    
+    
+    constructor() ERC721 ("TestNFT", "TestUNIQUE") {}
 
-    constructor() ERC721("TestNFT", "TestUNIQUE") {}
-
-    function mintNFT(address recipient, string memory tokenURI)
-        public onlyOwner
-        returns (uint256)
-    {
+    function mintNFT(address recipient, string memory tokenURI) public onlyOwner returns (uint256){
         _tokenIds.increment();
-
         uint256 newItemId = _tokenIds.current();
         _mint(recipient, newItemId);
         _setTokenURI(newItemId, tokenURI);
-
         return newItemId;
     }
+
+    function transferNFT(address sender, address recipient, uint256 tokenID) public virtual {
+        //solhint-disable-next-line max-line-length
+        require(_isApprovedOrOwner(_msgSender(), tokenID), "ERC721: transfer caller is not owner nor approved");
+
+        _transfer(sender, recipient, tokenID);
+    }
+
 }
