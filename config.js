779d608878b3c3a1d5ff808b1cceaf49dd408138
Antz0x0z <47379684+Antz0x0z@users.noreply.github.com>
2021-04-08 23:24:36 +0700

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-var config = {};
-
-config.API_URL = "https://eth-rinkeby.alchemyapi.io/v2/-mvqdyfsmKA4WDxmXeK0Iu_NZysBu2Y5";
-config.PRIVATE_KEY = "2be42de6b0a5e4454a7fa4be6fba4fc8016497ede0ef798bc1e3ee897bae429c";
-config.PUBLIC_KEY = "0xD3d7B7dfe325FDE51E93b566B7D247c024300Fb1";
-config.PIN_URL = "https://ipfs.io/ipfs/QmcXfMo1Luv4L8SP4uELHh4kdQsJzGJ62VvYBMdixzTWiv";
-config.CONTRACT_ADDRESS = "0x53a438Eb9fE322A25BC52d834C861B224DD7374B";
-
-module.exports = config;
