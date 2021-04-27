53c46606691e2099ced75177edb3b2b074865915
Antz0x0z <47379684+Antz0x0z@users.noreply.github.com>
2021-04-27 18:44:51 +0700

    Add ERC20 depo
    Add ERC20 contract deposit & withdraw

--- contracts/MyNFT.sol
+++ contracts/MyNFT.sol
@@ -6,22 +6,27 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-contract OwnableDelegateProxy {}
-
-contract ProxyRegistry {
-    mapping(address => OwnableDelegateProxy) public proxies;
-}
-
 contract MyNFT is ERC721, Ownable {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
     mapping(string => uint8) hashes;
     mapping (address => mapping (address => uint256)) internal allowed;
 
-    constructor() ERC721 ("T.NFT(m)", "T.UNIQUE(m)") {}
+    address public contractor;
+    address[] public byers;
     
+    constructor() ERC721 ("T.REC", "T.REX") {
+        contractor = msg.sender;
+    }
 
-    function mintNFT(address recipient, string memory tokenURI) public onlyOwner returns (uint256){
+    modifier Own() {
+        require(msg.sender == contractor);
+        _;
+    }  
+
+    function contribute() external payable {}  
+
+    function mintERC721(address recipient, string memory tokenURI) public onlyOwner returns (uint256){
         _tokenIds.increment();
         uint256 newItemId = _tokenIds.current();
         _mint(recipient, newItemId);
@@ -29,12 +34,23 @@ contract MyNFT is ERC721, Ownable {
         return newItemId;
     }
 
-    function transferNFT(address sender, address recipient, uint256 tokenID) public virtual {
+    function transferERC721(address sender, address recipient, uint256 tokenID) public virtual {
         //solhint-disable-next-line max-line-length
         require(_isApprovedOrOwner(_msgSender(), tokenID), "ERC721: transfer caller is not owner nor approved");
-
         _transfer(sender, recipient, tokenID);
         emit Transfer(msg.sender, recipient, tokenID);
     }
 
+    function getByers() public view returns (address[] memory) {
+        return byers;
+    }
+
+    function sendByers() public payable {
+        require(msg.value >= .001 ether);
+        byers.push(msg.sender);
+    }
+
+    function withdrawBalance (address payable recipient) public Own() payable {
+        recipient.transfer(address(this).balance);
+    }
 }
---
+++ scripts/send-to-contract.js
@@ -0,0 +1,34 @@
+async function main() {
+    var config = require('../config.js');
+    const { createAlchemyWeb3 } = require("@alch/alchemy-web3");
+    const web3 = createAlchemyWeb3(config.API_URL);
+    const contract = require("../artifacts/contracts/MyNFT.sol/MyNFT.json");
+    const contractAddress = config.CONTRACT_ADDRESS;
+    const nftContract = new web3.eth.Contract(contract.abi, contractAddress);
+    const sender = config.PUBLIC_KEY;
+    const privKey = config.PRIVATE_KEY;
+    // const recipient = config.RECIPIENT;;
+    const nonce = await web3.eth.getTransactionCount(sender, 'latest'); // nonce starts counting from 0
+
+    const tx = {
+     // 'to': '0x31B98D14007bDEe637298086988A0bBd31184523', // faucet address to return eth
+     // 'value': 1000000000000000000, // 1 ETH
+     'to': contractAddress, //payable contract address
+     'value': 100000000000000000, // 0.1 ETH
+     'gas': 70000, 
+     'nonce': nonce,
+     'data': nftContract.methods.sendByers().encodeABI()
+    };
+   
+    const signedTx = await web3.eth.accounts.signTransaction(tx, privKey);
+    
+    web3.eth.sendSignedTransaction(signedTx.rawTransaction, function(error, hash) {
+    if (!error) {
+      console.log("🎉 The hash of your transaction (0.1 ETH) is: ", hash);
+    } else {
+      console.log("❗Something went wrong while submitting your transaction:", error)
+    }
+   });
+}
+
+main();
